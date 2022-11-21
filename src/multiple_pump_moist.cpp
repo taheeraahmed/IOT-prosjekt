47d51555fde720e0ec15bc5f675f4c36ead43e68
Taheera Ahmed <60318348+taheeraahmed@users.noreply.github.com>
2022-11-21 12:59:50 +0100

    Prettier multiple moist pump B)

--- src/multiple_pump_moist.cpp
+++ src/multiple_pump_moist.cpp
@@ -1,7 +1,4 @@
-#include <Arduino.h>
-#include <ESP32_Servo.h>
-#include <map>
-#include <list>
+#include <utils.h>
 
 // Defining the necessary pins
 const int MOISTURE_PIN_1 = 39;
@@ -13,50 +10,6 @@ Servo servo;
 // Humidity threshold values
 const int lowThreshold = 20;
 const int highThreshold = 60;
-
-struct Plant {
-    int moisturePin;
-    int moistureValue;
-    int moisturePercent;
-} plant1, plant2, plant3;
-
-// Map-like function which constraints the input and output values
-int getPercent(int x, int in_min, int in_max, int out_min, int out_max) {
-    if (x > in_max) {
-        return out_max;
-    }
-    else if (x < in_min) {
-        return out_min;
-    } 
-    else {
-        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-    }
-}
-
-// Function which updates the moisture values 
-Plant updatePlantValues(Plant plant) {
-    const int airValue = 4094;
-    const int waterValue = 2800;
-
-    plant.moistureValue = analogRead(plant.moisturePin); 
-    plant.moisturePercent = getPercent(plant.moistureValue, waterValue, airValue,   100, 0);
-    return plant;
-}
-
-// Turn on pump
-void turnOnPump(Servo myServo, Plant plant, int highThreshold) {
-    int position = 0;
-    myServo.attach(PUMP_PIN);
-
-    while(plant.moisturePercent < highThreshold) {
-        myServo.write(position);
-        position++;
-        delay(20);
-        plant = updatePlantValues(plant);
-    }
-
-    myServo.detach();
-}
 
 void setup() {
     Serial.begin(9600); 
@@ -73,12 +26,12 @@ void loop() {
 
     // If the soil moisture is below the low threshold, turn on the pump
     if(plant1.moisturePercent < lowThreshold) {
-        turnOnPump(servo, plant1, highThreshold);
+        turnOnPump(servo, plant1, highThreshold, PUMP_PIN);
     }
     else if(plant2.moisturePercent < lowThreshold) {
-        turnOnPump(servo, plant2, highThreshold);
+        turnOnPump(servo, plant2, highThreshold, PUMP_PIN);
     }
     else if(plant3.moisturePercent < lowThreshold) {
-        turnOnPump(servo, plant3, highThreshold);
+        turnOnPump(servo, plant3, highThreshold, PUMP_PIN);
     }
 }
--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,13 @@
+#include <Arduino.h>
+#include <ESP32_Servo.h>
+
+struct Plant {
+    int moisturePin;
+    int moistureValue;
+    int moisturePercent;
+} plant1, plant2, plant3;
+
+
 // Map-like function which constraints the input and output values
 int getPercent(int x, int in_min, int in_max, int out_min, int out_max) {
     if (x > in_max) {
@@ -9,4 +19,32 @@ int getPercent(int x, int in_min, int in_max, int out_min, int out_max) {
     else {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
+}
+
+// Function which updates the moisture values 
+Plant updatePlantValues(Plant plant) {
+    const int airValue = 4094;
+    const int waterValue = 2800;
+
+    plant.moistureValue = analogRead(plant.moisturePin); 
+    plant.moisturePercent = getPercent(plant.moistureValue, waterValue, airValue,   100, 0);
+    Serial.println(plant.moisturePercent);
+    return plant;
+}
+
+// Turn on pump
+void turnOnPump(Servo myServo, Plant plant, int highThreshold, int PUMP_PIN) {
+    int position = 0;
+    myServo.attach(PUMP_PIN);
+
+    while(plant.moisturePercent < highThreshold) {
+        myServo.write(position);
+        position++;
+        delay(20);
+        plant = updatePlantValues(plant);
+        Serial.print("In while, moisture: ");
+        Serial.println(plant.moisturePercent);
+    }
+
+    myServo.detach();
 }
